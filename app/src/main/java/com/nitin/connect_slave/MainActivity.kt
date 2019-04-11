b8f9692ea07948f470e598d1cfc60a5758c4b847
Nitin <canvas.nv@gmail.com>
2019-04-11 13:52:03 +0530

    changed brightness

--- app/src/main/java/com/nitin/connect_slave/MainActivity.kt
+++ app/src/main/java/com/nitin/connect_slave/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.nitin.connect_slave
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
 
+const val WRITE_SETTINGS_PERMISSION_CODE = 124
+const val REQ_PICK_SOUNDFILE_CODE = 125
 class MainActivity : AppCompatActivity() {
     private lateinit var db: DatabaseReference
+    private lateinit var context: Context
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
         FirebaseApp.initializeApp(this)
 
         db = FirebaseDatabase.getInstance().reference.child("settings")
 
-        db.addValueEventListener(settingsListener)
+        if (!hasSettingsWritePermission()) {
+            requestWritePermission()
+        } else {
+            db.addValueEventListener(settingsListener)
+        }
     }
 
     override fun onDestroy() {
@@ -24,14 +41,61 @@ class MainActivity : AppCompatActivity() {
         db.removeEventListener(settingsListener)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == WRITE_SETTINGS_PERMISSION_CODE && hasSettingsWritePermission()) {
+            db.addValueEventListener(settingsListener)
+        }
+    }
+
     private val settingsListener = object : ValueEventListener {
         override fun onDataChange(dataSnapshot: DataSnapshot) {
             val settings = dataSnapshot.getValue(Settings::class.java)
             Log.d("Nitin", settings?.toString())
-        }
+            settings?.let {
+                updateBrightness(it.brightness)
 
+            }
+        }
         override fun onCancelled(error: DatabaseError) {
             Log.d("Nitin", "failed to read value", error.toException())
+        }
+    }
+
+    fun updateBrightness(level: Int) {
+        android.provider.Settings.System.putInt(
+            this@MainActivity.contentResolver,
+            android.provider.Settings.System.SCREEN_BRIGHTNESS,
+            level
+        )
+    }
+
+    fun hasSettingsWritePermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            android.provider.Settings.System.canWrite(context)
+        } else {
+            return (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_SETTINGS
+            ) == PackageManager.PERMISSION_GRANTED)
+        }
+    }
+
+    private fun requestWritePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            intent = Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS)
+            intent.data = Uri.parse("package:${context.packageName}")
+
+            this@MainActivity.startActivityForResult(
+                intent,
+                WRITE_SETTINGS_PERMISSION_CODE
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.WRITE_SETTINGS),
+                WRITE_SETTINGS_PERMISSION_CODE
+            )
         }
     }
 }
