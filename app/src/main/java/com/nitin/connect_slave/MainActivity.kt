51a9fe21179fd035785c00936c8b9cbf39fa05fc
Nitin <canvas.nv@gmail.com>
2019-04-11 12:29:15 +0530

    getting data from firebase

--- app/src/main/java/com/nitin/connect_slave/MainActivity.kt
+++ app/src/main/java/com/nitin/connect_slave/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.nitin.connect_slave
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var db: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this)
+
+        db = FirebaseDatabase.getInstance().reference.child("settings")
+
+        db.addValueEventListener(settingsListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        db.removeEventListener(settingsListener)
+    }
+
+    private val settingsListener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            val settings = dataSnapshot.getValue(Settings::class.java)
+            Log.d("Nitin", settings?.toString())
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Log.d("Nitin", "failed to read value", error.toException())
+        }
     }
 }
---
+++ app/src/main/java/com/nitin/connect_slave/Settings.kt
@@ -0,0 +1,9 @@
+package com.nitin.connect_slave
+
+data class Settings(
+    val brightness: Int = 20,
+    val state: String = "pause",
+    val volume: Int = 20
+) {
+
+}
